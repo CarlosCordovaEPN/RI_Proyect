75d9d86a7ab7e60e50282ae218b976b0c8e619ce
Carlos Córdova <108033929+CarlosCordovaEPN@users.noreply.github.com>
2024-11-21 13:58:58 -0500

    Limpieza del texto

--- RAG Reuters/src/preprocessing.py
+++ RAG Reuters/src/preprocessing.py
@@ -1,17 +1,7 @@
 import os
 
 def read_reuters_files(directory_path='../data/reuters/training'):
-    """
-    Read all files from the Reuters training directory
-    
-    Args:
-        directory_path (str): Path to the Reuters training directory
-        
-    Returns:
-        list: List of tuples containing (filename, content)
-    """
     documents = []
-    
     try:
         # Get list of all files in directory
         for filename in os.listdir(directory_path):
